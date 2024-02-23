6c48b472d7ffdb24fe01411ba46c9a9fd1cdccd4
Davide Ponzini <davide.ponzini95@gmail.com>
2024-02-23 11:07:20 +0100

    imports

--- index.php
+++ index.php
@@ -9,16 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website portfolio</title>
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- Boostrap icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
-
-    <!-- Custom -->
-    <link rel="stylesheet" href="/styles/_common.css">
-    <link rel="stylesheet" href="/styles/style.css">
-    <link rel="stylesheet" href="/styles/social.css">
+    <!-- Imports -->
+    <?php include('../components/imports.html'); ?>
 
     <!-- Policies -->
     <?php include('components/head_policies.html'); ?>
