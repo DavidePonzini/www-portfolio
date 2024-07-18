01a36df780761715979198b7625f27bdbcf204fb
Davide Ponzini <davide.ponzini95@gmail.com>
2024-07-18 16:11:41 +0200

    updated style

--- index.php
+++ index.php
@@ -20,49 +20,47 @@
     <?php include('../components/navbar.html'); ?>
     
     <div class="parallax" style="background-image: url(/res/server-bg.png)">
-        <div class="container">
-            <div class="content-tab">
-                <h1>Websites</h1>
-                <div class="row center">
-                    <div class="col-lg links">
-                        <a href="2016-sito-brutto/" class="card">
-                            <img class="card-img-top" src="2016-sito-brutto/antonio.jpg">
-                            <div class="card-body">
-                                <h5 class="card-title">Sito brutto</h5>
-                                <p class="card-text">Badly designed webpage, using only HTML and CSS (2016).</p>
-                                <span class="btn btn-primary">Open</span>
-                            </div>
-                        </a>
+        <div class="container-md container-dark">
+            <h1>Websites</h1>
+            <div class="row center">
+                <div class="col-lg links">
+                    <a href="2016-sito-brutto/" class="card">
+                        <img class="card-img-top" src="2016-sito-brutto/antonio.jpg">
+                        <div class="card-body">
+                            <h5 class="card-title">Sito brutto</h5>
+                            <p class="card-text">Badly designed webpage, using only HTML and CSS (2016).</p>
+                            <span class="btn btn-primary">Open</span>
+                        </div>
+                    </a>
 
-                        <a href="2018-scenarios-from-the-future/" class="card">
-                            <img class="card-img-top" src="2018-scenarios-from-the-future/res/chart-linechart.png">
-                            <div class="card-body">
-                                <h5 class="card-title">Scenarios from the Future</h5>
-                                <p class="card-text">Visualization tools for complex data, developed as final project of Data Visualization course (2018).</p>
-                                <span class="btn btn-primary">Open</span>
-                            </div>
-                        </a>
-                    
-                        <a href="2019-chicken-chasers/" class="card">
-                            <img class="card-img-top" src="2019-chicken-chasers/res/chicken_kicking.jpg">
-                            <div class="card-body">
-                                <h5 class="card-title">Chicken Chasers</h5>
-                                <p class="card-text">Chicken-based website, developed as didactic exercise during my first year teaching Computer Science (2019).</p>
-                                <span class="btn btn-primary">Open</span>
-                            </div>
-                        </a>
-                    
-                        <a href="2022-web-tutorial/" class="card">
-                            <img class="card-img-top" src="2022-web-tutorial/res/DSC_0653b-1024x1536.jpg"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Danicucina Web Tutorial</h5>
-                                <p class="card-text">Websited based on recipes from <span href="https://blog.giallozafferano.it/danicucina/">Danicucina</span>, to teach website developement (2022).</p>
-                                <span class="btn btn-primary">Open</span>
-                            </div>
-                        </a>
-                    
+                    <a href="2018-scenarios-from-the-future/" class="card">
+                        <img class="card-img-top" src="2018-scenarios-from-the-future/res/chart-linechart.png">
+                        <div class="card-body">
+                            <h5 class="card-title">Scenarios from the Future</h5>
+                            <p class="card-text">Visualization tools for complex data, developed as final project of Data Visualization course (2018).</p>
+                            <span class="btn btn-primary">Open</span>
+                        </div>
+                    </a>
+                
+                    <a href="2019-chicken-chasers/" class="card">
+                        <img class="card-img-top" src="2019-chicken-chasers/res/chicken_kicking.jpg">
+                        <div class="card-body">
+                            <h5 class="card-title">Chicken Chasers</h5>
+                            <p class="card-text">Chicken-based website, developed as didactic exercise during my first year teaching Computer Science (2019).</p>
+                            <span class="btn btn-primary">Open</span>
+                        </div>
+                    </a>
+                
+                    <a href="2022-web-tutorial/" class="card">
+                        <img class="card-img-top" src="2022-web-tutorial/res/DSC_0653b-1024x1536.jpg"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Danicucina Web Tutorial</h5>
+                            <p class="card-text">Websited based on recipes from <span href="https://blog.giallozafferano.it/danicucina/">Danicucina</span>, to teach website developement (2022).</p>
+                            <span class="btn btn-primary">Open</span>
+                        </div>
+                    </a>
+                
 
-                    </div>
                 </div>
             </div>
         </div>
